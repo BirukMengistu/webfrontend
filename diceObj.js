ef68a83412c536d0ceb62758c64a6ae53b527c23
BirukMengistu <sheger.shelf3@gmail.com>
2017-10-26 16:03:28 +0200

    js , jquery and css files

---
+++ diceObj.js
@@ -0,0 +1,9 @@
+var dice = {
+side = 6;
+roll = function()
+{
+var randomNumber = Math.floor(Math.random()*this.sides)+1;
+return randomNumber;
+alert("well come to the Dice; Game!")
+ }
+}
---
+++ jquery.js
@@ -0,0 +1,3 @@
+$("li").on("click" , function()
+{$(this).text("Clicked").delay(800).fadeout(400);
+});
